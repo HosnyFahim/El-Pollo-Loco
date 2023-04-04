4e9ed4d1db1a6723b64f2348c6f9497966b80332
HosnyFahim <hosny.fahim1990@gmail.com>
2023-04-04 22:51:23 +0200

    keyBoard function fixed

--- classes/keyboard.class.js
+++ classes/keyboard.class.js
@@ -10,29 +10,28 @@ class Keyboard {
 
 
 /* Listening to the keyboard and setting the keyboard object to true when the key is pressed. */
-window.addEventListener('keydown', (event) => {
-    if (event.keyCode == 39) {
+window.addEventListener("keydown", (e) => {
+    if (e.code == "ArrowRight") {
         keyboard.RIGHT = true;
     }
 
-    if (event.keyCode == 37) {
+    if (e.code == "ArrowLeft") {
         keyboard.LEFT = true;
     }
 
-    if (event.keyCode == 38) {
+    if (e.code == "ArrowUp") {
         keyboard.UP = true;
     }
 
-    if (event.keyCode == 40) {
+    if (e.code == "ArrowDown") {
         keyboard.DOWN = true;
     }
 
-    if (event.keyCode == 32) {
+    if (e.code == "Space") {
         keyboard.SPACE = true;
     }
 
-
-});
+})
 
 /* Listening to the keyboard and setting the keyboard object to false when the key is released. */
 window.addEventListener('keyup', (event) => {

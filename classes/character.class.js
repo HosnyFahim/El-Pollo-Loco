64a39c17a72f439a06cc73a2e349dd1712c3ba2d
HosnyFahim <hosny.fahim1990@gmail.com>
2023-04-05 20:12:40 +0200

    added images Sleep charakter

--- classes/character.class.js
+++ classes/character.class.js
@@ -18,6 +18,19 @@ class Character extends MovableObject {
         'img/2_character_pepe/1_idle/idle/I-10.png'
     ];
 
+    IMAGES_SLEEP = [
+        'img/2_character_pepe/1_idle/long_idle/I-11.png',
+        'img/2_character_pepe/1_idle/long_idle/I-12.png',
+        'img/2_character_pepe/1_idle/long_idle/I-13.png',
+        'img/2_character_pepe/1_idle/long_idle/I-14.png',
+        'img/2_character_pepe/1_idle/long_idle/I-15.png',
+        'img/2_character_pepe/1_idle/long_idle/I-16.png',
+        'img/2_character_pepe/1_idle/long_idle/I-17.png',
+        'img/2_character_pepe/1_idle/long_idle/I-18.png',
+        'img/2_character_pepe/1_idle/long_idle/I-19.png',
+        'img/2_character_pepe/1_idle/long_idle/I-20.png'
+    ];
+
     IMAGES_WALKING = [
         'img/2_character_pepe/2_walk/W-21.png',
         'img/2_character_pepe/2_walk/W-22.png',
@@ -119,7 +132,6 @@ class Character extends MovableObject {
                 else {
                     this.playAnimation(this.IMAGES_IDLE);
                 }
-            
         }, 100);
     }
 
